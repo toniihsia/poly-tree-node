767a4d42e8565cd78e4c334bfed326a8f8887d76
App Academy Student <student@appacademy.io>
2016-09-16 11:45:38 -0700

    implement dfs and bfs

--- lib/00_tree_node.rb
+++ lib/00_tree_node.rb
@@ -30,4 +30,27 @@ class PolyTreeNode
     self.children.delete(child_node)
     child_node.parent=(nil)
   end
+
+  def dfs(target_value)
+    return self if self.value == target_value
+
+    @children.each do |child|
+      value = child.dfs(target_value)
+      return value if value
+    end
+
+    nil
+  end
+
+  def bfs(target_value)
+    q = [self]
+
+    until q.empty?
+      node = q.shift
+      return node if node.value == target_value
+      q += node.children
+    end
+
+    nil
+  end
 end
